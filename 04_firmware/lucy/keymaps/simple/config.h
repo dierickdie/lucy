9c39c7b6f31ab9dfbc9c3615c10a593739f8c1b2
Rick <therick042@gmail.com>
2022-04-11 17:01:51 -0500

    update simple keymap.c

---
+++ 04_firmware/lucy/keymaps/simple/config.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+// place overrides here
+
+//#define HOLD_ON_OTHER_KEY_PRESS
+
+#define IGNORE_MOD_TAP_INTERRUPT
+
+//#define TAPPING_TERM_PER_KEY
+
+//#define PERMISSIVE_HOLD
+
+//#define PERMISSIVE_HOLD_PER_KEY
+
+#define TAPPING_TERM 200
