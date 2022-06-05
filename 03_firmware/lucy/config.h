35de704263147e314b6547a1b79e0dacc9c10c24
Rick <therick042@gmail.com>
2022-06-05 11:33:45 -0500

    update firmware
    added magicboot

--- 03_firmware/lucy/config.h
+++ 03_firmware/lucy/config.h
@@ -16,11 +16,17 @@
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 13
 
-//#define MATRIX_ROW_PINS { F7, F6, D4, D6 } // origg
-//#define MATRIX_COL_PINS { D7, D5, B7, F5, F4, F1, F0, E6, B0, B1, B2, B3,    C6} // ori
-//#define MATRIX_COL_PINS { D7, D5, B7, B3, B2, B1, B0, E6, F0, F1, F4, F5,    C6 } // therick48
-#define MATRIX_ROW_PINS { B2, F0, E6, F1 } // lucy
-#define MATRIX_COL_PINS { B0, B1, B3, D4, D6, D7, B4, B5, B6, C6, C7, F7,    F6} // lucy
+//lucy
+#define MATRIX_ROW_PINS { B2, F0, E6, F1 }
+#define MATRIX_COL_PINS { B0, B1, B3, D4, D6, D7, B4, B5, B6, C6, C7, F7,    F6}
+
+//ori
+//#define MATRIX_ROW_PINS { F7, F6, D4, D6 }
+//#define MATRIX_COL_PINS { D7, D5, B7, F5, F4, F1, F0, E6, B0, B1, B2, B3,    C6}
+
+//therick48
+//#define MATRIX_ROW_PINS { F7, F6, D4, D6 }
+//#define MATRIX_COL_PINS { D7, D5, B7, F5, F4, F1, F0, E6, B0, B1, B2, B3,    C6 }
 
 #define UNUSED_PINS
 
@@ -42,6 +48,9 @@
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
+
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 0
 
 // QMK-DFU
 //#define QMK_ESC_OUTPUT F1  // COL pin if COL2ROW
